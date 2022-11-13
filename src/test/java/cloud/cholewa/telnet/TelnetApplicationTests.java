26d6c8520a7e2a7b0034b815ad9362b4ab9ee4a7
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2022-11-13 11:44:15 +0100

    initial commit

---
+++ src/test/java/cloud/cholewa/telnet/TelnetApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.cholewa.telnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelnetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
